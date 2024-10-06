8867fbabd2805a5e6d2d35fb645dd0ddd5feaa0f
winncoder <thangtnsa000@gmail.com>
2024-10-07 02:18:34 +0700

    fix(artall): fix  grpc port for health check

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-
-  // Khởi động HTTP server trước
   await app.listen(3001);
-  console.log('HTTP server is running on http://localhost:3001');
-
-  // Kết nối microservice gRPC
   app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
   await app.startAllMicroservices();
-
-  // Cấu hình pipes và exception filter toàn cục
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
