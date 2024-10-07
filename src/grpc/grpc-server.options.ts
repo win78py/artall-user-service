4207bbd7ea112ff52129b4759b033670a8f22081
winncoder <thangtnsa000@gmail.com>
2024-10-07 23:27:40 +0700

    fix(artall): fix  grpc port for health check

--- src/grpc/grpc-server.options.ts
+++ src/grpc/grpc-server.options.ts
@@ -11,7 +11,7 @@ export const grpcClientOptions: ClientProviderOptions = {
       join(__dirname, '../grpc/users.proto'),
       join(__dirname, '../grpc/health.proto'),
     ],
-    url: 'localhost:50051',
+    url: 'artall-services-932712333.us-east-1.elb.amazonaws.com:50051',
     maxReceiveMessageLength: 20 * 1024 * 1024,
     maxSendMessageLength: 20 * 1024 * 1024,
   },
