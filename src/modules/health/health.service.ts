18e2dfb250ddb153661523971f3feac32c915ad3
winncoder <thangtnsa000@gmail.com>
2024-10-08 21:15:02 +0700

    fix(artall): fix  grpc port for health

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -1,25 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import {
-  HealthCheckRequest,
-  HealthCheckResponse,
-  ServingStatus,
-} from '../../common/interface/health.interface';
+import { HealthCheckResponse } from '../../common/interface/health.interface';
 
 @Injectable()
 export class HealthService {
-  check(request: HealthCheckRequest): HealthCheckResponse {
-    const response: HealthCheckResponse = {
-      status: ServingStatus.SERVING,
+  check(): HealthCheckResponse {
+    return {
+      message: 'Hello Artallers!',
     };
-
-    // Logic to check service health
-    if (request.service === 'USER_SERVICE') {
-      // Logic kiểm tra trạng thái của service user
-      response.status = ServingStatus.SERVING;
-    } else {
-      response.status = ServingStatus.UNKNOWN;
-    }
-
-    return response;
   }
 }
